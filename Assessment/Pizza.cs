ce53c10114dc82d8550841ef99bff5d2b629f49d
u29d58 <u29d58@INGURWN120082>
2023-11-03 09:45:09 +0530

    Add project files.

---
+++ Assessment/Pizza.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment
+{
+    public class Pizza
+    {
+        private string size;
+        private int cheeseToppings;
+        private int pepperoniToppings;
+        private int hamToppings;
+
+        // Constructor
+        public Pizza(string size, int cheeseToppings, int pepperoniToppings, int hamToppings)
+        {
+            this.size = size;
+            this.cheeseToppings = cheeseToppings;
+            this.pepperoniToppings = pepperoniToppings;
+            this.hamToppings = hamToppings;
+        }
+
+        // Properties using get and set methods
+        public string Size
+        {
+            get { return size; }
+            set { size = value; }
+        }
+
+        public int CheeseToppings
+        {
+            get { return cheeseToppings; }
+            set { cheeseToppings = value; }
+        }
+
+        public int PepperoniToppings
+        {
+            get { return pepperoniToppings; }
+            set { pepperoniToppings = value; }
+        }
+
+        public int HamToppings
+        {
+            get { return hamToppings; }
+            set { hamToppings = value; }
+        }
+
+        // Method to calculate the cost of the pizza
+        public double CalcCost()
+        {
+            double cost = 0;
+
+            switch (size.ToLower())
+            {
+                case "small":
+                    cost = 10 + 2 * (cheeseToppings + pepperoniToppings + hamToppings);
+                    break;
+                case "medium":
+                    cost = 12 + 2 * (cheeseToppings + pepperoniToppings + hamToppings);
+                    break;
+                case "large":
+                    cost = 14 + 2 * (cheeseToppings + pepperoniToppings + hamToppings);
+                    break;
+                default:
+                    Console.WriteLine("Invalid size");
+                    break;
+            }
+
+            return cost;
+        }
+
+        // Method to get the description of the pizza
+        public string GetDescription()
+        {
+            return $"{size} pizza with {cheeseToppings} cheese toppings, {pepperoniToppings} pepperoni toppings, and {hamToppings} ham toppings.";
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
---
+++ Assessment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example usage
+            Pizza pizza1 = new Pizza("medium", 2, 1, 1);
+            Pizza pizza2 = new Pizza("large", 3, 2, 0);
+
+            Console.WriteLine("Pizza 1: " + pizza1.GetDescription());
+            Console.WriteLine("Cost of Pizza 1: $" + pizza1.CalcCost());
+
+            Console.WriteLine("\nPizza 2: " + pizza2.GetDescription());
+            Console.WriteLine("Cost of Pizza 2: $" + pizza2.CalcCost());
+        }
+    }
+
+}
+
